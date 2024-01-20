664fa36095f93cb0be9b0d298e38610a6b42f0c8
Krzysztof Kasprowicz <krzysztof@impe.pl>
2024-01-20 21:10:28 +0100

    mm

--- GeminiOpenAIConversation/Program.cs
+++ GeminiOpenAIConversation/Program.cs
@@ -1,6 +1,4 @@
-﻿// See https://aka.ms/new-console-template for more information
-
-using Microsoft.Extensions.Configuration;
+﻿using Microsoft.Extensions.Configuration;
 using Microsoft.SemanticKernel;
 using Microsoft.SemanticKernel.ChatCompletion;
 using Microsoft.SemanticKernel.Services;
