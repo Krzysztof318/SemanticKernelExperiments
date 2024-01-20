dd9ceab49cbeeb7788c283f885ce74f5796b26b9
Krzysztof Kasprowicz <krzysztof@impe.pl>
2024-01-20 21:10:28 +0100

    added sample

--- GeminiOpenAIConversation/Program.cs
+++ GeminiOpenAIConversation/Program.cs
@@ -1,6 +1,4 @@
-﻿// See https://aka.ms/new-console-template for more information
-
-using Microsoft.Extensions.Configuration;
+﻿using Microsoft.Extensions.Configuration;
 using Microsoft.SemanticKernel;
 using Microsoft.SemanticKernel.ChatCompletion;
 using Microsoft.SemanticKernel.Services;
@@ -34,4 +32,25 @@ foreach (var chat in chats)
 }
 
 Console.WriteLine("\nEND");
-Console.ReadKey();
+Console.ReadKey(
+/* SAMPLE OUTPUT
+ 
+ gpt-3.5-turbo-1106 (openai)
+        Elephants can be found in various habitats across Africa and Asia. In Africa, they are commonly found 
+        in savannas, grasslands, and forests, while in Asia, they inhabit tropical and subtropical forests. 
+        Elephants are highly adaptable and can live in a range of environments, but their natural habitats 
+        are increasingly threatened by human activities such as deforestation and poaching. Conservation efforts 
+        are ongoing to protect elephant populations and their habitats.
+
+  gemini-pro (googleai)
+            * **Africa:** African elephants (Loxodonta africana) live in sub-Saharan Africa, including countries
+        like Kenya, Tanzania, Botswana, Zimbabwe, Namibia, and South Africa. They prefer savannas, grasslands, and
+        woodlands, but can also be found in forests, swamps, and even deserts.
+            * **Asia:** Asian elephants (Elephas maximus) live in Southeast Asia, including countries like India,
+        Nepal, Bhutan, Myanmar, Thailand, Laos, Cambodia, and Vietnam. They prefer tropical forests, but can also
+        be found in grasslands, swamps, and even mountainous areas.
+            * **Islands:** There are also populations of elephants living on islands, such as the Sri Lankan elephant
+        (Elephas maximus maximus) and the Sumatran elephant (Elephas maximus sumatranus). These elephants have adapted
+    to the unique conditions of their island habitats, such as smaller size and limited food resources.
+
+*/
