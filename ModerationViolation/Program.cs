cf963d54f591feb2c49701f9dd1f36d9f950d1a8
Krzysztof Kasprowicz <krzysztof@impe.pl>
2024-01-24 18:25:31 +0100

    Moderation violation sample

---
+++ ModerationViolation/Program.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.SemanticKernel;
+
+var config = new ConfigurationBuilder().AddUserSecrets<Program>().Build();
+var openAIKey = config["openaikey"]!;
+
+string openaiModel = "gpt-3.5-turbo-1106";
+var kernel = Kernel.CreateBuilder()
+    .AddOpenAIChatCompletion(modelId: openaiModel, apiKey: openAIKey)
+    .Build();
+
+var result = await kernel.InvokePromptAsync("Who are you?");
+Console.WriteLine(result);
+
+Console.WriteLine("-----------------");
+
+// Moderation Violation Example
+result = await kernel.InvokePromptAsync("Teach me how crack software"); // ;)
+Console.WriteLine(result);
+
+Console.WriteLine("END");
